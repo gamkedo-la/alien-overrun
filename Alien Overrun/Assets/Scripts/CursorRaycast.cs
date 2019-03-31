69c24a4d02dba454d4f51442f814e754a5168faa
bilalCh213 <boyz213@live.com>
2019-03-31 15:22:05 +0500

    Refactored the code (CursorRaycast.cs)

--- Alien Overrun/Assets/Scripts/CursorRaycast.cs
+++ Alien Overrun/Assets/Scripts/CursorRaycast.cs
@@ -7,12 +7,15 @@ public class CursorRaycast : MonoBehaviour
 {
 	public GameObject hoverSelectionInfoUI;
 	public GameObject lockedSelectionInfoUI;
+	[Space]
 
 	public Material defaultCursorMaterial;
-	public Material goodCursorMaterial;
-	public Material badCursorMaterial;
+	public Material hoverCursorMaterial;
+	public Material lockedCursorMaterial;
+	[Space]
 
 	public GameObject lockedSelectionIndicator;
+	[Space]
 
 	[SerializeField] private GameObject cursorIndicator;
 	[SerializeField] private Transform pointOfPlane = null;
@@ -38,81 +41,13 @@ public class CursorRaycast : MonoBehaviour
 
 		rend = cursorIndicator.GetComponent<MeshRenderer>();
 
-		hoverInfo1 = hoverSelectionInfoUI.transform.GetChild(1).GetComponent<TextMeshProUGUI>();
-		hoverInfo2 = hoverSelectionInfoUI.transform.GetChild(2).GetComponent<TextMeshProUGUI>();
-
-		lockedInfo1 = lockedSelectionInfoUI.transform.GetChild(1).GetComponent<TextMeshProUGUI>();
-		lockedInfo2 = lockedSelectionInfoUI.transform.GetChild(2).GetComponent<TextMeshProUGUI>();
+		GetUIComponents();
 	}
 	
     void Update()
     {
-		Ray mRay = Camera.main.ScreenPointToRay(Input.mousePosition);
-
-		if (plane.Raycast(mRay, out float mouseDistance))
-		{
-			transform.position = mRay.GetPoint(mouseDistance);
-
-			if (lockedSelection != null)
-			{
-				cursorIndicator.transform.position = mRay.GetPoint(mouseDistance);
-
-				Building building = lockedSelection.GetComponent<Building>();
-				HP hp = lockedSelection.GetComponent<HP>();
-
-				lockedInfo1.text = "Building: " + building.BuildingName + "\nBuild Cost: " + building.BuildCost + "\nPlace Distance:" + building.PlaceDistance;
-				lockedInfo2.text = "Hit Points: " + hp.MaxHP + "/" + hp.CurrentHP + "\nBuild Time: " + building.BuildTime;
-			}
-			else if (hoverSelection != null)
-			{
-				cursorIndicator.transform.position = hoverSelection.transform.position;
-
-				Building building = hoverSelection.GetComponent<Building>();
-				HP hp = hoverSelection.GetComponent<HP>();
-
-				hoverInfo1.text = "Building: " + building.BuildingName + "\nBuild Cost: " + building.BuildCost;
-				hoverInfo2.text = "Hit Points: " + hp.MaxHP + "/" + hp.CurrentHP + "\nBuild Time: " + building.BuildTime;
-			}
-			else
-			{
-				cursorIndicator.transform.position = mRay.GetPoint(mouseDistance);
-			}
-		}
-
-		if (Input.GetMouseButtonDown(0))
-		{
-			if (hoverSelection != null)
-			{
-				if(lockedSelection != null)
-					lockedSelection.GetComponent<Building>().Indicator.HideRange();
-
-				lockedSelection = hoverSelection;
-				rend.material = goodCursorMaterial;
-
-				hoverSelectionInfoUI.SetActive(false);
-				lockedSelectionInfoUI.SetActive(true);
-
-				if(selectionIndicator != null)
-					DestroyImmediate(selectionIndicator);
-				selectionIndicator = null;
-
-				selectionIndicator = Instantiate(lockedSelectionIndicator, lockedSelection.transform.position, Quaternion.Euler(0f, 0f, 0f));
-				lockedSelection.GetComponent<Building>().Indicator.ShowRange(true);
-			}
-			else if(lockedSelection != null)
-			{
-				lockedSelection.GetComponent<Building>().Indicator.HideRange();
-
-				lockedSelection = null;
-				rend.material = defaultCursorMaterial;
-				
-				lockedSelectionInfoUI.SetActive(false);
-
-				if (selectionIndicator != null)
-					DestroyImmediate(selectionIndicator);
-				selectionIndicator = null;
-			}
-		}
+		UpdateCursorPositionAndEntityInfo();
+		SelectionControl();
 	}
 
 	private void OnTriggerEnter(Collider collision)
@@ -125,7 +60,7 @@ public class CursorRaycast : MonoBehaviour
 			if (lockedSelection == null)
 			{
 				hoverSelectionInfoUI.SetActive(true);
-				rend.material = badCursorMaterial;
+				rend.material = hoverCursorMaterial;
 			}
 		}
 	}
@@ -142,6 +77,104 @@ public class CursorRaycast : MonoBehaviour
 				hoverSelectionInfoUI.SetActive(false);
 				rend.material = defaultCursorMaterial;
 			}
+		}
+	}
+
+
+
+	private void GetUIComponents()
+	{
+		hoverInfo1 = hoverSelectionInfoUI.transform.GetChild(1).GetComponent<TextMeshProUGUI>();
+		hoverInfo2 = hoverSelectionInfoUI.transform.GetChild(2).GetComponent<TextMeshProUGUI>();
+
+		lockedInfo1 = lockedSelectionInfoUI.transform.GetChild(1).GetComponent<TextMeshProUGUI>();
+		lockedInfo2 = lockedSelectionInfoUI.transform.GetChild(2).GetComponent<TextMeshProUGUI>();
+	}
+
+
+
+	private void UpdateCursorPositionAndEntityInfo()
+	{
+		Ray mRay = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+		if (plane.Raycast(mRay, out float mouseDistance))
+		{
+			transform.position = mRay.GetPoint(mouseDistance);
+
+			if (lockedSelection != null)
+			{
+				cursorIndicator.transform.position = mRay.GetPoint(mouseDistance);
+				SetInfo(true);
+			}
+			else if (hoverSelection != null)
+			{
+				cursorIndicator.transform.position = hoverSelection.transform.position;
+				SetInfo();
+			}
+			else
+				cursorIndicator.transform.position = mRay.GetPoint(mouseDistance);
+		}
+	}
+
+
+
+	private void SelectionControl()
+	{
+		if (Input.GetMouseButtonDown(0))
+		{
+			if (hoverSelection != null)
+			{
+				if (lockedSelection != null)
+					lockedSelection.GetComponent<Building>().Indicator.HideRange();
+
+				lockedSelection = hoverSelection;
+				rend.material = lockedCursorMaterial;
+
+				hoverSelectionInfoUI.SetActive(false);
+				lockedSelectionInfoUI.SetActive(true);
+
+				if (selectionIndicator != null)
+					DestroyImmediate(selectionIndicator);
+				selectionIndicator = null;
+
+				selectionIndicator = Instantiate(lockedSelectionIndicator, lockedSelection.transform.position, Quaternion.Euler(0f, 0f, 0f));
+				lockedSelection.GetComponent<Building>().Indicator.ShowRange(true);
+			}
+			else if (lockedSelection != null)
+			{
+				lockedSelection.GetComponent<Building>().Indicator.HideRange();
+
+				lockedSelection = null;
+				rend.material = defaultCursorMaterial;
+
+				lockedSelectionInfoUI.SetActive(false);
+
+				if (selectionIndicator != null)
+					DestroyImmediate(selectionIndicator);
+				selectionIndicator = null;
+			}
+		}
+	}
+
+
+
+	private void SetInfo( bool isLocked = false )
+	{
+		if (!isLocked)
+		{
+			Building building = hoverSelection.GetComponent<Building>();
+			HP hp = hoverSelection.GetComponent<HP>();
+
+			hoverInfo1.text = "Building: " + building.BuildingName + "\nBuild Cost: " + building.BuildCost;
+			hoverInfo2.text = "Hit Points: " + hp.MaxHP + "/" + hp.CurrentHP + "\nBuild Time: " + building.BuildTime;
+		}
+		else
+		{
+			Building building = lockedSelection.GetComponent<Building>();
+			HP hp = lockedSelection.GetComponent<HP>();
+
+			lockedInfo1.text = "Building: " + building.BuildingName + "\nBuild Cost: " + building.BuildCost + "\nPlace Distance:" + building.PlaceDistance;
+			lockedInfo2.text = "Hit Points: " + hp.MaxHP + "/" + hp.CurrentHP + "\nBuild Time: " + building.BuildTime;
 		}
 	}
 }
