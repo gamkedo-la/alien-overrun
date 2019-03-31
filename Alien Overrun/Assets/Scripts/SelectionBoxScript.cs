0a4589c8e35020de1896b1ca7c2d9903509294fb
bilalCh213 <boyz213@live.com>
2019-03-31 16:02:55 +0500

    Added Selection Box for selecting Multiple Entities (not functional, drag mouse feature only)

---
+++ Alien Overrun/Assets/Scripts/SelectionBoxScript.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SelectionBoxScript : MonoBehaviour
+{
+	public Transform boxTransform;
+
+	[SerializeField] private Transform pointOfPlane = null;
+	
+	private Plane plane;
+
+	private Vector3 startPosition = Vector3.zero;
+	private Vector3 endPosition = Vector3.zero;
+
+	void Start()
+    {
+		Vector3 upVector = Vector3.up;
+		plane = new Plane(upVector, pointOfPlane.position);
+	}
+
+	void Update()
+	{
+		Ray mRay = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+		if (plane.Raycast(mRay, out float mouseDistance))
+			boxTransform.position = mRay.GetPoint(mouseDistance);
+
+		if (Input.GetMouseButtonUp(0))
+		{
+			startPosition = Vector3.zero;
+			boxTransform.localScale = new Vector3(0f, boxTransform.localScale.y, 0f);
+		}
+		else if (startPosition != Vector3.zero)
+		{
+			endPosition = boxTransform.position;
+
+			boxTransform.localScale = new Vector3(
+				startPosition.x - endPosition.x,
+				boxTransform.localScale.y,
+				startPosition.z - endPosition.z
+			);
+
+			boxTransform.position = new Vector3(
+				startPosition.x - (boxTransform.localScale.x / 2f),
+				boxTransform.position.y,
+				startPosition.z - (boxTransform.localScale.z / 2f)
+			);
+		}
+		else if (Input.GetMouseButtonDown(0))
+		{
+			startPosition = boxTransform.position;
+		}
+	}
+
+}
