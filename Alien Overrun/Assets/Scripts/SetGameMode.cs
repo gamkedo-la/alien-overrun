54aa216e9701d8b572093f117b4189626c66ed26
KornelGM <KORNik_k@wp.pl>
2019-04-04 13:09:42 +0200

    Fixed game mode selection (after returning to main menu) and pause (also after returning to main menu)

---
+++ Alien Overrun/Assets/Scripts/SetGameMode.cs
@@ -0,0 +1,17 @@
+﻿/**
+ * Description: Sets the game mode.
+ * Authors: Kornel
+ * Copyright: © 2019 Kornel. All rights reserved. For license see: 'LICENSE.txt'
+ **/
+
+using UnityEngine;
+
+public class SetGameMode : MonoBehaviour
+{
+	public void SetMode( bool creativeMode )
+	{
+		ModeSelection modeSelection = FindObjectOfType<ModeSelection>( );
+		if ( modeSelection )
+			modeSelection.CreativeMode = creativeMode;
+	}
+}
