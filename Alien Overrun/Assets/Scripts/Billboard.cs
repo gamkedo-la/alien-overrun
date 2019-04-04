0c773f829ec20c797b5c4c01d257f3ce0b5cc5bd
KornelGM <KORNik_k@wp.pl>
2019-04-04 10:09:47 +0200

    Created simple HP bars for the enemies and attached them

---
+++ Alien Overrun/Assets/Scripts/Billboard.cs
@@ -0,0 +1,32 @@
+﻿/**
+ * Description: Faces objects towards the camera but retains their X rotation.
+ * Authors: Kornel
+ * Copyright: © 2019 Kornel. All rights reserved. For license see: 'LICENSE.txt'
+ **/
+
+using UnityEngine;
+using UnityEngine.Assertions;
+
+public class Billboard : MonoBehaviour
+{
+	private Camera cam = null;
+
+	void Start ()
+	{
+		cam = Camera.main;
+		Assert.IsNotNull( cam );
+	}
+
+	void LateUpdate( )
+	{
+		transform.LookAt
+		(
+			transform.position + cam.transform.rotation * Vector3.back,
+			cam.transform.rotation * Vector3.down
+		);
+
+		Vector3 angles = transform.eulerAngles;
+		angles.x = 0;
+		transform.eulerAngles = angles;
+	}
+}
