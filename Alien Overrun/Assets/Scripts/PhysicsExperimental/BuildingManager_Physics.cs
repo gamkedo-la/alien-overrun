1f925d2ed5e54a0e194be23ab8184d2d36f058d0
bilalCh213 <boyz213@live.com>
2019-04-11 22:24:12 +0500

    Experimental: Enemy-Tower Interaction with added Physics (not working, under progress)

---
+++ Alien Overrun/Assets/Scripts/PhysicsExperimental/BuildingManager_Physics.cs
@@ -0,0 +1,79 @@
+﻿/**
+ * Description: Manages buildings, provides functionality for other scripts.
+ * Authors: Kornel
+ * Copyright: © 2019 Kornel. All rights reserved. For license see: 'LICENSE.txt'
+ **/
+
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class BuildingManager_Physics : MonoBehaviour
+{
+	public static BuildingManager_Physics Instance { get; private set; }
+
+	public List<Building_Physics> Buildings { get; private set; }
+
+	[SerializeField] private bool building = false;
+	public bool Building { get { return building; } set { building = value; } }
+
+	private void Awake()
+	{
+		Buildings = new List<Building_Physics>();
+
+		if (Instance != null && Instance != this)
+			Destroy(gameObject);
+		else
+			Instance = this;
+	}
+
+	private void OnDestroy() { if (this == Instance) { Instance = null; } }
+
+	public void AddBuilding(Building_Physics building)
+	{
+		Buildings.Add(building);
+	}
+
+	public void RemoveBuilding(Building_Physics building)
+	{
+		Buildings.Remove(building);
+	}
+
+	public void ShowZones(bool show)
+	{
+		foreach (var building in Buildings)
+			building.ShowPlaceZone(show);
+	}
+
+	public Vector3 GetNearestCoreOrZero(Vector3 position)
+	{
+		Vector3 returnPos = Vector3.zero;
+
+		float distance = 100000000;
+		foreach (var building in Buildings)
+			if (building.BuildingType == BuildingPhysicsType.Core &&
+				 Vector3.Distance(building.transform.position, position) < distance)
+			{
+				distance = Vector3.Distance(building.transform.position, position);
+				returnPos = building.transform.position;
+			}
+
+		return returnPos;
+	}
+
+	public bool CanPlaceBuiding(Building_Physics buildingToPlace)
+	{
+		foreach (var building in Buildings)
+		{
+			if (building.AreWeCloseEnough(buildingToPlace))
+				return true;
+		}
+
+		return false;
+	}
+
+	public int CoresLeft()
+	{
+		return Buildings.Select(b => b).Where(b => b.BuildingType == BuildingPhysicsType.Core).Count();
+	}
+}
---
+++ Alien Overrun/Assets/Scripts/PhysicsExperimental/Building_Physics.cs
@@ -0,0 +1,125 @@
+﻿/**
+ * Description: Main class of all buildings. Responsible for core behaviors.
+ * Authors: Kornel
+ * Copyright: © 2019 Kornel. All rights reserved. For license see: 'LICENSE.txt'
+ **/
+
+using UnityEngine;
+using UnityEngine.Assertions;
+
+public enum BuildingPhysicsType
+{
+	Core,
+	Tower,
+	Wall
+}
+
+public class Building_Physics : MonoBehaviour
+{
+	public Indicator Indicator { get { return indicator; } private set { indicator = value; } }
+	[SerializeField] private Indicator indicator = null;
+
+	public BuildingPhysicsType BuildingType { get { return buildingType; } private set { buildingType = value; } }
+	[SerializeField] private BuildingPhysicsType buildingType = BuildingPhysicsType.Tower;
+
+	public string BuildingName { get { return buildingName; } private set { buildingName = value; } }
+	[SerializeField] private string buildingName = "Building";
+
+	public int BuildCost { get { return buildCost; } private set { buildCost = value; } }
+	[SerializeField] private int buildCost = 100;
+
+	public float PlaceDistance { get { return placeDistance; } private set { placeDistance = value; } }
+	[SerializeField] private float placeDistance = 6;
+
+	public float BuildTime { get { return buildTime; } private set { buildTime = value; } }
+	[SerializeField] private float buildTime = 1.0f;
+
+	[SerializeField] private Collider col = null;
+	[SerializeField] private Collider colP = null;
+	[SerializeField] private Behaviour[] toEnableOnBuild = null;
+	[SerializeField] private bool enableOnStart = false;
+
+	private int collisions = 0;
+
+	void Start()
+	{
+		Assert.IsNotNull(indicator);
+		Assert.IsNotNull(col);
+		Assert.IsNotNull(colP);
+		Assert.IsNotNull(toEnableOnBuild);
+		Assert.AreNotEqual(toEnableOnBuild.Length, 0);
+
+		indicator.HideAll();
+
+		if (enableOnStart)
+			EnableBuilding();
+	}
+
+	void OnDisable()
+	{
+		if (BuildingManager_Physics.Instance)
+			BuildingManager_Physics.Instance.RemoveBuilding(this);
+	}
+
+	void OnMouseDown()
+	{
+		//GetComponentInChildren<MeshRenderer>( ).material.color = Random.ColorHSV( );
+		Debug.Log($"Clicked: {name}");
+	}
+
+
+	void OnMouseEnter()
+	{
+		//The Range is shown when selected (locked, not hover)
+		/*
+		if ( !LevelManager.Instance.Paused )
+			indicator.ShowRange( true );
+		*/
+	}
+
+	void OnMouseExit()
+	{
+		//The Range gets hidden when deselected (locked, not hover)
+		//indicator.HideRange( );
+	}
+
+	void OnTriggerEnter(Collider other)
+	{
+		if (CollidesWithTags(other.gameObject))
+			collisions++;
+	}
+
+	void OnTriggerExit(Collider other)
+	{
+		if (CollidesWithTags(other.gameObject))
+			collisions--;
+	}
+
+	public void EnableBuilding()
+	{
+		BuildingManager_Physics.Instance.AddBuilding(this);
+		col.isTrigger = false;
+
+		foreach (var item in toEnableOnBuild)
+			item.enabled = true;
+
+		colP.enabled = true;
+	}
+
+	public bool CanBePaced() => collisions == 0;
+
+	public void ShowPlaceZone(bool show) => indicator.ShowZone(show);
+
+	public void ShowRange(bool canBuild) => indicator.ShowRange(canBuild);
+	public void HideRange() => indicator.HideRange();
+
+	public bool AreWeCloseEnough(Building_Physics anotherBuilding)
+		=> Vector3.Distance(transform.position, anotherBuilding.gameObject.transform.position) <= placeDistance;
+
+	private bool CollidesWithTags(GameObject gameObject)
+	{
+		return gameObject.CompareTag(Tags.Building) ||
+			   gameObject.CompareTag(Tags.Enemy) ||
+			   gameObject.CompareTag(Tags.Environment);
+	}
+}
