4ffb3177a4d769231c3a006f5e971bd25a31e815
KornelGM <KORNik_k@wp.pl>
2019-04-11 17:47:28 +0200

    Added new projectile - Fireball (based on the original Cannonball) - now affects rigid bodies but does not yet do direct damage and clearly is not balanced :D

--- Alien Overrun/Assets/Scripts/Combat/AreaOfEffectDamager.cs
+++ Alien Overrun/Assets/Scripts/Combat/AreaOfEffectDamager.cs
@@ -5,28 +5,74 @@
  **/
 
 using UnityEngine;
-using UnityEngine.Assertions;
 
 [RequireComponent( typeof( SphereCollider ) )]
 public class AreaOfEffectDamager : MonoBehaviour
 {
+	[Header("Damage")]
 	[SerializeField] private int damage = 12;
+	[SerializeField] private DamageType damageType = DamageType.Fire;
+	[Header("Propagation")]
 	[SerializeField] private float startSize = 1f;
 	[SerializeField] private float endSize = 5f;
 	[SerializeField] private float time = 2f;
-	[SerializeField] private DamageType damageType = DamageType.Fire;
+	[Header("Force")]
+	[SerializeField] private float explosionForce = 1000f;
 
 	private float currnetSize = 0;
 	private float currnetTime = 0;
 
 	void Start ()
 	{
-		//Assert.IsNotNull(  );
 		currnetSize = startSize;
 		transform.localScale = Vector3.one * currnetSize;
 	}
 
 	void Update ()
+	{
+		Grow( );
+	}
+
+	private void OnCollisionEnter( Collision collision )
+	{
+		//Debug.Log( "Collision damage with: " + collision.gameObject.name );
+		DoDamage( collision.gameObject );
+		ApplyForce( collision.gameObject );
+	}
+
+	private void OnTriggerEnter( Collider other )
+	{
+		//Debug.Log( "Trigger damage with: " + other.gameObject.name );
+		DoDamage( other.gameObject );
+		ApplyForce( other.gameObject );
+	}
+
+	private void DoDamage( GameObject other )
+	{
+		if ( other.transform.CompareTag( Tags.Enemy ) )
+		{
+			HP hp = other.gameObject.GetComponent<HP>( );
+			if ( hp )
+			{
+				float damg = damage * Interactions.GetMultiplier( damageType, hp.Resistance );
+				hp.ChangeHP( -damg );
+				Utilities.DrawDebugText( other.transform.position + Vector3.up, damg.ToString( ) );
+			}
+		}
+	}
+
+	private void ApplyForce( GameObject other )
+	{
+		Rigidbody rb = other.gameObject.GetComponent<Rigidbody>( );
+		if ( rb )
+		{
+			// Force decreases with distance (current size of the explosion)
+			//float force = maxForce * ( 1.1f - transform.localScale.x / endSize );
+			rb.AddExplosionForce( explosionForce, transform.position, endSize, 0, ForceMode.Impulse );
+		}
+	}
+
+	private void Grow( )
 	{
 		currnetTime += Time.deltaTime;
 		currnetSize = startSize + ( ( endSize - startSize ) * ( currnetTime / time ) );
@@ -34,37 +80,5 @@ public class AreaOfEffectDamager : MonoBehaviour
 
 		if ( currnetTime >= time )
 			Destroy( gameObject );
-	}
-
-	private void OnCollisionEnter( Collision collision )
-	{
-		//Debug.Log( "Collision damage with: " + collision.gameObject.name );
-
-		if ( collision.transform.CompareTag( Tags.Enemy ) )
-		{
-			HP hp = collision.gameObject.GetComponent<HP>( );
-			if ( hp != null )
-			{
-				float damg = damage * Interactions.GetMultiplier( damageType, hp.Resistance );
-				hp.ChangeHP( -damg );
-				Utilities.DrawDebugText( collision.transform.position + Vector3.up, damg.ToString( ) );
-			}
-		}
-	}
-
-	private void OnTriggerEnter( Collider other )
-	{
-		//Debug.Log( "Trigger damage with: " + other.gameObject.name );
-
-		if ( other.transform.CompareTag( Tags.Enemy ) )
-		{
-			HP hp = other.gameObject.GetComponent<HP>( );
-			if ( hp != null )
-			{
-				float damg = damage * Interactions.GetMultiplier( damageType, hp.Resistance );
-				hp.ChangeHP( -damg );
-				Utilities.DrawDebugText( other.transform.position + Vector3.up, damg.ToString( ) );
-			}
-		}
 	}
 }
