30cb84fc227d8af781aae8fe99c4a646d0da5a26
bilalCh213 <boyz213@live.com>
2019-04-13 15:43:34 +0500

    Experiment: Worked a bit more on Enemy-Tower Interaction (moving parts, not working, under progress)

---
+++ Alien Overrun/Assets/Enemy_Physics.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy_Physics : MonoBehaviour
+{
+	public float speed = 5f;
+
+	public GameObject[] cores;
+
+	public TriggerTarget triggerTarget;
+
+	private GameObject target = null;
+
+    void Start()
+    {
+        
+    }
+	
+    void Update()
+    {
+		GetTarget();
+
+		Vector3 dir = (target.transform.position - transform.position).normalized;
+		transform.position += dir * Time.deltaTime * speed;
+	}
+
+	void GetTarget()
+	{
+		if (triggerTarget.GetTarget() == null)
+			target = cores[GetNearestCore()];
+		else
+			target = triggerTarget.GetTarget();
+	}
+
+	int GetNearestCore()
+	{
+		float leastDistance = 999999f;
+		int index = -1;
+
+		for (int i = 0; i < cores.Length; i++)
+		{
+			float dist = Vector3.Distance(cores[i].transform.position, transform.position);
+			if (leastDistance > dist)
+			{
+				leastDistance = dist;
+				index = i;
+			}
+		}
+			
+		return index;
+	}
+
+	private void OnTriggerEnter(Collider other)
+	{
+		
+	}
+}
--- Alien Overrun/Assets/Scripts/PhysicsExperimental/ProjectileShooter.cs
+++ Alien Overrun/Assets/Scripts/PhysicsExperimental/ProjectileShooter.cs
@@ -16,12 +16,12 @@ public class ProjectileShooter : MonoBehaviour
 	[SerializeField] private float reloadTime = 2f;
 	[SerializeField] private int ammoCost = 0;
 	[Space]
-	[SerializeField, Range(0, 300)] private float shootingForce = 3.7f;
+	[SerializeField] private float shootingForce = 3.7f;
 	//[SerializeField] private float upAngleCorrection = -90f;
 	//[SerializeField] private float arrowTimePerUnit = 0.09f;
 	[SerializeField] private float verticalSpread = 5f;
 	[SerializeField] private float horizontalSpread = 5f;
-
+	
 	private float timeToNextShot = 0f;
 	private Transform target = null;
 
@@ -75,22 +75,20 @@ public class ProjectileShooter : MonoBehaviour
 
 		//ResourceManager.Instance.UseResources(ResourceType.Minerals, ammoCost);
 
-		Vector3 standardTower = transform.position;
+		Vector3 standardTower = transform.parent.position;
 		standardTower.y = 0;
 		Vector3 standardEnemy = target.position;
 		standardEnemy.y = 0;
 
 		Vector3 dir = (target.position - spawnPoint.position).normalized;
-		
-		/*
-		float angle = Utilities.AngleBetweenVectors(new Vector2(transform.position.x, transform.position.z), new Vector2(target.transform.position.x, target.transform.position.z));
-		spawnPointPivot.rotation = Quaternion.Euler(0, angle + 180, 0);
+
+		float angle = Utilities.AngleBetweenVectors(new Vector2(transform.parent.position.x, transform.parent.position.z), new Vector2(target.transform.position.x, target.transform.position.z));
+		spawnPointPivot.rotation = Quaternion.Euler(0, -angle + 90, 0);
 
 		// Some randomness
 		dir = Quaternion.AngleAxis(Random.Range(-horizontalSpread, horizontalSpread), Vector3.Cross(Vector3.down, dir)) * dir;
 		dir = Quaternion.AngleAxis(Random.Range(-verticalSpread, verticalSpread), Vector3.Cross(Vector3.right, dir)) * dir;
-		*/
-
+		
 		Vector3 force = dir * (shootingForce);
 		var go = Instantiate(projectile, spawnPoint.position, Quaternion.identity);
 		go.GetComponent<Rigidbody>().AddForce(force);
@@ -98,19 +96,13 @@ public class ProjectileShooter : MonoBehaviour
 
 	private void OnTriggerStay(Collider other)
 	{
-		if (other.gameObject.tag == "Enemy")
-		{
-			if (target == null)
-				target = other.gameObject.transform;
-		}
+		if (target == null && other.gameObject.tag == "Enemy")
+			target = other.gameObject.transform;
 	}
 
 	private void OnTriggerExit(Collider other)
 	{
-		if (other.gameObject.tag == "Enemy")
-		{
-			if (target != null)
-				target = null;
-		}
+		if (target != null && other.gameObject.tag == "Enemy")
+			target = null;
 	}
 }
