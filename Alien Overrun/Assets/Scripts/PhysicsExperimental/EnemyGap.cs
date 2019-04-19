34503e87eca939c2d3f1d5e8275d4dadcb0cf864
bilalCh213 <boyz213@live.com>
2019-04-19 21:43:35 +0500

    Physics Experimental: Enemies will try to maintain some gap between each other.

---
+++ Alien Overrun/Assets/Scripts/PhysicsExperimental/EnemyGap.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyGap : MonoBehaviour
+{
+	private void OnCollisionStay(Collision collision)
+	{
+		Vector3 dir = collision.contacts[0].point - transform.position;
+		dir = -dir.normalized;
+		dir = new Vector3(dir.x, 0f, dir.z);
+		transform.parent.parent.position += dir * 0.1f;
+	}
+}
