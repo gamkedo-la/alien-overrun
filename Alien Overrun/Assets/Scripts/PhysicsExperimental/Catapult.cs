95e8a49fae7da3772fb09c47c706af5a8e701fd5
Christer Kaitila <breakdance@mcfunkypants.com>
2019-04-20 18:00:03 -0700

    catapult hinge joint experiments (not functional)

--- Alien Overrun/Assets/Scripts/PhysicsExperimental/Catapult.cs
+++ Alien Overrun/Assets/Scripts/PhysicsExperimental/Catapult.cs
@@ -1,18 +1,37 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Assertions;
 
 public class Catapult : MonoBehaviour
 {
+
+    public GameObject spawnOnHit = null;
+    public float launchForce = 10f;
+    private Rigidbody rb;
+    private HingeJoint hinge;
+
+    private void OnCollisionEnter( Collision collision )
+	{
+		if(collision.gameObject.CompareTag("Enemy")) {
+
+            Debug.Log("Catapult touched an Enemy! Turning on motor!");
+            if (spawnOnHit) Instantiate(spawnOnHit, collision.contacts[0].point, Quaternion.identity);
+            hinge.useMotor = true;
+            // FIXME: turn off in 1 second, reset position
+        }
+	}
+    
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = gameObject.GetComponent<Rigidbody>();
+        hinge = gameObject.GetComponent<HingeJoint>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        //if (rb) rb.AddTorque(transform.right * launchForce); // rotate
     }
 }
