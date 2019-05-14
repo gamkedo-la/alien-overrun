bed595973be27e49270a84ee98d7ae1b44afc738
KornelGM <KORNik_k@wp.pl>
2019-05-14 12:39:38 +0200

    Castle slowly generates resources, so the player does not get stuck with no option to do anything

---
+++ Alien Overrun/Assets/Scripts/ResourceGenerator.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+public class ResourceGenerator : MonoBehaviour
+{
+	[SerializeField] private ResourceType resourceType = ResourceType.Minerals;
+	[SerializeField] private int amount = 5;
+	[SerializeField] private float interval = 3;
+
+	void Start( )
+	{
+		Invoke( "GenerateResources", interval );
+	}
+
+	private void GenerateResources( )
+	{
+		ResourceManager.Instance.AddResources( ResourceType.Minerals, amount );
+
+		Invoke( "GenerateResources", interval );
+	}
+}
