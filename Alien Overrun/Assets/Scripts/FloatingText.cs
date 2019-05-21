7fc35c8d955194de44b9e396c88178672c49c14b
KornelGM <KORNik_k@wp.pl>
2019-05-21 09:41:16 +0200

    Updated buttons and game title in main menu

---
+++ Alien Overrun/Assets/Scripts/FloatingText.cs
@@ -0,0 +1,22 @@
+﻿/**
+ * Description: Floating Text control class.
+ * Authors: Kornel
+ * Copyright: © 2019 Kornel. All rights reserved. For license see: 'LICENSE.txt'
+ **/
+
+using UnityEngine;
+using UnityEngine.Assertions;
+
+public class FloatingText : MonoBehaviour
+{
+	void Start ()
+	{
+		//Assert.IsNotNull( , $"Please assign <b></b> field: <b>{GetType( ).Name}</b> script on <b>{name}</b> object" );
+
+	}
+
+	void Update ()
+	{
+
+	}
+}
