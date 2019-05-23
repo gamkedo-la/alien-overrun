c7b8d7b062f19577025824b00d74120d6b3131b3
KornelGM <KORNik_k@wp.pl>
2019-05-23 17:50:56 +0200

    Added tutorial in to the game

---
+++ Alien Overrun/Assets/Scripts/ShowHide.cs
@@ -0,0 +1,33 @@
+﻿/**
+ * Description: Shows and/or on enabled/disabled.
+ * Authors: Kornel
+ * Copyright: © 2019 Kornel. All rights reserved. For license see: 'LICENSE.txt'
+ **/
+
+using UnityEngine;
+
+public class ShowHide : MonoBehaviour
+{
+	public GameObject[] ToShowOnEnabled = null;
+	public GameObject[] ToShowOnDisabled = null;
+	public GameObject[] ToHideOnEnabled = null;
+	public GameObject[] ToHideOnDisabled = null;
+
+	private void OnEnable( )
+	{
+		foreach ( var item in ToShowOnEnabled )
+			item.SetActive( true );
+
+		foreach ( var item in ToHideOnEnabled )
+			item.SetActive( false );
+	}
+
+	private void OnDisable( )
+	{
+		foreach ( var item in ToShowOnDisabled )
+			item.SetActive( true );
+
+		foreach ( var item in ToHideOnDisabled )
+			item.SetActive( false );
+	}
+}
