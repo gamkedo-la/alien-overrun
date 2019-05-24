bbff384dd17ed2d6dd9e62f6f3017175122930d7
KornelGM <KORNik_k@wp.pl>
2019-05-24 13:59:19 +0200

    Added a way to skip the tutorial and to never show it again, fixed visual issues and spelling errors, added new tutorial message (about Aliens)

---
+++ Alien Overrun/Assets/Scripts/SetPosition.cs
@@ -0,0 +1,23 @@
+﻿/**
+ * Description: Sets position of a transform.
+ * Authors: Kornel
+ * Copyright: © 2019 Kornel. All rights reserved. For license see: 'LICENSE.txt'
+ **/
+
+using UnityEngine;
+using UnityEngine.Assertions;
+
+public class SetPosition : MonoBehaviour
+{
+	public Transform Target = null;
+	public Vector3 Position = Vector3.zero;
+	void Start ()
+	{
+		Assert.IsNotNull( Target, $"Please assign <b>Target</b> field: <b>{GetType( ).Name}</b> script on <b>{name}</b> object" );
+	}
+
+	public void OnPress( )
+	{
+		Target.position = Position;
+	}
+}
---
+++ Alien Overrun/Assets/Scripts/Tutorial.cs
@@ -0,0 +1,40 @@
+﻿/**
+ * Description: Controls the tutorial
+ * Authors: Kornel
+ * Copyright: © 2019 Kornel. All rights reserved. For license see: 'LICENSE.txt'
+ **/
+
+using UnityEngine;
+using UnityEngine.Assertions;
+
+public class Tutorial : MonoBehaviour
+{
+	[SerializeField] private GameObject tutorialObject = null;
+
+	void Start ()
+	{
+		Assert.IsNotNull( tutorialObject, $"Please assign <b>TutorialObject</b> field: <b>{GetType( ).Name}</b> script on <b>{name}</b> object" );
+
+		if ( PlayerPrefs.GetInt( "Tutorial", 1 ) == 1 )
+			tutorialObject.SetActive( true );
+	}
+
+	public void Checkbox (bool value)
+	{
+		if ( value )
+			NeverShow( );
+		else
+			AlwaysShow( );
+	}
+
+	public void NeverShow ()
+	{
+		PlayerPrefs.SetInt( "Tutorial", 0 );
+	}
+
+	[ContextMenu( "Always Show" )]
+	public void AlwaysShow( )
+	{
+		PlayerPrefs.SetInt( "Tutorial", 1 );
+	}
+}
