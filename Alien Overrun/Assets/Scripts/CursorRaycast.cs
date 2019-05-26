a2ee32b0894e43aee63adeb30c47b2b456d453d9
KornelGM <KORNik_k@wp.pl>
2019-05-26 18:32:06 +0200

    Place distance only shown for Core and Castle

--- Alien Overrun/Assets/Scripts/CursorRaycast.cs
+++ Alien Overrun/Assets/Scripts/CursorRaycast.cs
@@ -315,6 +315,8 @@ public class CursorRaycast : MonoBehaviour
 				HP hp = hoverSelection.GetComponent<HP>();
 
 				hoverInfo1.text = $"Building: {building.BuildingName}\nBuild Cost: {building.BuildCostMinerals}M {building.BuildCostCrystals}C";
+				if ( building.BuildingType == BuildingType.Castle || building.BuildingType == BuildingType.Core )
+					hoverInfo1.text += $"\nBuild range: { building.PlaceDistance}";
 				hoverInfo2.text = "Hit Points: " + hp.MaxHP + "/" + hp.CurrentHP;// + "\nBuild Time: " + building.BuildTime;
 			}
 			else
@@ -336,7 +338,9 @@ public class CursorRaycast : MonoBehaviour
 			{
 				HP hp = lockedSelection[0].GetComponent<HP>();
 
-				lockedInfo1.text = $"Building: {building.BuildingName}\nBuild Cost: {building.BuildCostMinerals}M {building.BuildCostCrystals}C\nPlace Distance: {building.PlaceDistance}";
+				lockedInfo1.text = $"Building: {building.BuildingName}\nBuild Cost: {building.BuildCostMinerals}M {building.BuildCostCrystals}C";
+				if ( building.BuildingType == BuildingType.Castle || building.BuildingType == BuildingType.Core )
+					lockedInfo1.text += $"\nBuild range: { building.PlaceDistance}";
 				lockedInfo2.text = "Hit Points: " + Mathf.FloorToInt( hp.MaxHP ) + "/" + Mathf.FloorToInt( hp.CurrentHP );// + "\nBuild Time: " + building.BuildTime;
 			}
 			else
