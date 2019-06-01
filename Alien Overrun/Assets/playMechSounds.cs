4e36edc2a7052ed4f7d8db8a00646c712bad34c7
stebssbets <stebssbets@yahoo.com>
2019-06-01 15:06:30 -0400

    scuttler movement sounds and mech movement sounds reimplemented into their respective move animations on their prefabs, as previously mentioned the mech sounds are a bit noisy and perhaps placing a counter variable on them to only play every 3 animation loop would help clean it up

---
+++ Alien Overrun/Assets/playMechSounds.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playMechSounds : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void playMechCreakOneShot()
+    {
+        FMODUnity.RuntimeManager.PlayOneShot("event:/MechCreak");
+    }
+
+    public void playMechStepOneShot()
+    {
+        FMODUnity.RuntimeManager.PlayOneShot("event:/MechStep");
+    }
+
+}
