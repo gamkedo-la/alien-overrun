ff9bf6f5715368d9305f62bf6e073a2a7f897bfb
stebssbets <stebssbets@yahoo.com>
2019-06-02 12:35:18 -0400

    created mineral mining wavs and put them in the MineralMining FMOD event, created MinerExistsSoPlaySound script and put it on the MineralMiner prefab but it doesn't work. I can pick it up tomorrow or someone can take a look at it

---
+++ Alien Overrun/Assets/MinerExistsSoPlaySound.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MinerExistsSoPlaySound : MonoBehaviour
+{
+    
+    public int NumberOfMineralMiners = 0;
+    private FMOD.Studio.EventInstance MineralMiningSound = FMODUnity.RuntimeManager.CreateInstance("event:/MineralMining");
+    FMOD.Studio.PLAYBACK_STATE MineralMiningSoundPlaybackState;
+    // Start is called before the first frame update
+    void Start()
+    {
+        NumberOfMineralMiners++;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        MineralMiningSound.getPlaybackState(out MineralMiningSoundPlaybackState);
+
+        if (NumberOfMineralMiners > 0 /*&& MineralMiningSoundPlaybackState != FMOD.Studio.PLAYBACK_STATE.PLAYING*/)
+        {
+            MineralMiningSound.start();
+        }  
+        if (NumberOfMineralMiners == 0)
+        {
+            MineralMiningSound.stop(FMOD.Studio.STOP_MODE.ALLOWFADEOUT);
+        }
+    }
+
+    private void OnDestroy()
+    {
+        NumberOfMineralMiners--;
+    }
+}
