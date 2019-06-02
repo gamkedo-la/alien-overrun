b67d0a9379ba4f94dba5ab5bbc8d476a798476ac
KornelGM <KORNik_k@wp.pl>
2019-06-02 16:40:36 +0200

    Added vol control for FMOD and cleaned up a script

--- Alien Overrun/Assets/Scripts/playMechSounds.cs
+++ Alien Overrun/Assets/Scripts/playMechSounds.cs
@@ -1,42 +1,28 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class playMechSounds : MonoBehaviour
 {
-    public int mechAnimationLoopCount;
+	[SerializeField] private int mechAnimationLoopCount = 0;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        mechAnimationLoopCount = 0;
-    }
+	public void increaseMechAnimationLoopCount( )
+	{
+		mechAnimationLoopCount++;
+		if ( mechAnimationLoopCount == 100 )
+		{
+			mechAnimationLoopCount = 0;
+		}
+	}
 
-    // Update is called once per frame
-    void Update()
-    {
-        if (mechAnimationLoopCount == 100)
-        {
-            mechAnimationLoopCount = 0;
-        }
-    }
+	public void playMechCreakOneShot( )
+	{
+		FMODUnity.RuntimeManager.PlayOneShot( "event:/MechCreak" );
+	}
 
-    public void increaseMechAnimationLoopCount()
-    {
-        mechAnimationLoopCount++;
-    }
-
-    public void playMechCreakOneShot()
-    {
-        FMODUnity.RuntimeManager.PlayOneShot("event:/MechCreak");
-    }
-
-    public void playMechStepOneShot()
-    {
-        if (mechAnimationLoopCount == 1)
-        {
-            FMODUnity.RuntimeManager.PlayOneShot("event:/MechStep");
-        }
-    }
-
+	public void playMechStepOneShot( )
+	{
+		if ( mechAnimationLoopCount == 1 )
+		{
+			FMODUnity.RuntimeManager.PlayOneShot( "event:/MechStep" );
+		}
+	}
 }
