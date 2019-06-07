f5e8467fb789f7d578fcee53a0037ff57acd2ce6
stebssbets <stebssbets@yahoo.com>
2019-06-07 18:18:23 -0400

    frogAlienSteps recorded, put in FMOD, turned down a bit and limited the number of instances to 2 with stealing behavior changed to none

--- Alien Overrun/Assets/Scripts/playFrogAlienSounds.cs
+++ Alien Overrun/Assets/Scripts/playFrogAlienSounds.cs
@@ -4,39 +4,9 @@ using UnityEngine;
 
 public class playFrogAlienSounds : MonoBehaviour
 {
-	public int mechAnimationLoopCount;
-
-	// Start is called before the first frame update
-	void Start( )
-	{
-		mechAnimationLoopCount = 0;
-	}
-
-	// Update is called once per frame
-	void Update( )
-	{
-		if ( mechAnimationLoopCount == 100 )
-		{
-			mechAnimationLoopCount = 0;
-		}
-	}
-
-	public void increaseMechAnimationLoopCount( )
-	{
-		mechAnimationLoopCount++;
-	}
-
-	public void playMechCreakOneShot( )
-	{
-		//FMODUnity.RuntimeManager.PlayOneShot( "event:/MechCreak" );
-	}
-
-	public void playMechStepOneShot( )
-	{
-		if ( mechAnimationLoopCount == 1 )
-		{
-			//FMODUnity.RuntimeManager.PlayOneShot( "event:/MechStep" );
-		}
-	}
+	public void playFrogAlienStep()
+    {
+        FMODUnity.RuntimeManager.PlayOneShot("event:/Enemies/Frog Alien/frogAlienSteps");
+    }
 
 }
