6c1dac5e5aebf46024c9ecc7066926ca3c171475
stebssbets <stebssbets@yahoo.com>
2019-06-10 19:26:26 -0400

    frog attack sound created and implemented onto the attack animation clip of the frog prefab

--- Alien Overrun/Assets/Scripts/playFrogAlienSounds.cs
+++ Alien Overrun/Assets/Scripts/playFrogAlienSounds.cs
@@ -8,6 +8,16 @@ public class playFrogAlienSounds : MonoBehaviour
 
 	public void PlaySpecialSound( ) { }
 
+    public void PlayAttackSound() => PlayFrogAlienStompOneShot();
+    
+        
+    
+
+    public void PlayFrogAlienStompOneShot()
+    {
+        FMODUnity.RuntimeManager.PlayOneShot("event:/Enemies/Frog Alien/FrogAlienStomp");
+    }
+
 	public void IncreaseCounter( ) { }
 
 	private void playFrogAlienStep()
